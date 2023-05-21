c98960bafc3c367fd49488a39d0d83b297bc52cc
Christian Meneses <christian.meneses.ls@gmail.com>
2023-05-21 19:44:03 +0200

    update

--- src/service/api.js
+++ src/service/api.js
@@ -1,20 +1,20 @@
-import axios from 'axios';
-const API_URL = 'https://flask-production-4716.up.railway.app/wisdomtoothai';
+import axios from 'axios'
+const API_URL = import.meta.env.VITE_API_URL
 
 export async function postUser(input) {
-  const source = axios.CancelToken.source();
+  const source = axios.CancelToken.source()
 
   const config = {
     timeout: 5000,
     cancelToken: source.token
-  };
+  }
 
-  const body = JSON.stringify({ user: input });
+  const body = JSON.stringify({ user: input })
 
   try {
-    const response = await axios.post(API_URL, body, config).then(data => data.data);
-    return { type: 'answer', content: response.data };
+    const response = await axios.post(API_URL, body, config).then(data => data.data)
+    return { type: 'answer', content: response.data }
   } catch (error) {
-    return { type: 'answer', content: 'Some error has occurred' };
+    return { type: 'answer', content: 'Some error has occurred' }
   }
 }
